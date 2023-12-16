732de0fd4f076f9120de7a708f667fd6abcf48da
Chris <lilunlogic@163.com>
2023-12-16 12:02:24 +0800

    update crp design pattern and corrent some code

--- src/main/java/com/chris/design/pattern/crp/dynamic/GetewayClient.java
+++ src/main/java/com/chris/design/pattern/crp/dynamic/GetewayClient.java
@@ -3,6 +3,7 @@ package com.chris.design.pattern.crp.dynamic;
 public class GetewayClient {
     public static void main(String[] args) {
         GatewayHandler firstGetewayHandler = GatewayHandlerEnumFactory.getFirstGatewayHandler();
+        assert firstGetewayHandler != null;
         firstGetewayHandler.service();
     }
 }
